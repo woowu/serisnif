d77571298fda039ed0f148c06e5cf9972eb60e11
Woody Wu <woody.wu2@landisgyr.com>
2022-08-21 14:04:29 +0800

    refine

--- src/serisnif.js
+++ src/serisnif.js
@@ -19,7 +19,8 @@ class SerialListener extends EventEmitter {
         });
         this._name = device + (name ? ' ' + name : '');
         this.timer = null;
-        this.buf = null;
+        this._chunk = null;
+        this._offset = 0;
 
         this.serial.open(err => {
             if (err) {
@@ -29,13 +30,19 @@ class SerialListener extends EventEmitter {
         });
         this.serial.on('data', data => {
             clearTimeout(this.timer);
-            if (! this.buf)
-                this.buf = data;
+            if (! this._chunk)
+                this._chunk = {
+                    time: new Date(),
+                    offset: this._offset,
+                    data,
+                }
             else
-                this.buf = Buffer.concat([this.buf, data]);
+                this._chunk.data = Buffer.concat([this._chunk.data, data]);
+
             this.timer = setTimeout(() => {
-                this.emit('data', this.buf);
-                this.buf = null;
+                this._offset = this._chunk.offset + this._chunk.data.length;
+                this.emit('data', this._chunk);
+                this._chunk = null;
             }, interframeTimeout);
         });
     }
@@ -106,22 +113,36 @@ const argv = require('yargs/yargs')(process.argv.slice(2))
         describe: 'log file',
         type: 'string',
     })
+    .option('left-name', {
+        alias: 'L',
+        describe: 'left device name',
+        type: 'string',
+        default: 'left',
+    })
+    .option('right-name', {
+        alias: 'L',
+        describe: 'right device name',
+        type: 'string',
+        default: 'right',
+    })
     .argv;
 
 const left = argv.left
-    ? new SerialListener(argv.left, argv.baud, argv.interFrameTimeout, 'left') : null;
+    ? new SerialListener(argv.left, argv.baud, argv.interFrameTimeout, argv.leftName) : null;
 const right = argv.right
-    ? new SerialListener(argv.right, argv.baud, argv.interFrameTimeout, 'right') : null;
+    ? new SerialListener(argv.right, argv.baud, argv.interFrameTimeout, argv.rightName) : null;
 const log = argv.log
     ? makeLogger(argv.log) : null;
 
 [left, right].forEach(dev => {
     if (! dev) return;
-    dev.on('data', data => {
-        const now = new Date();
-        console.log(dev.name());
+    dev.on('data', ({ time, offset, data }) => {
+        const timestamp = moment(time).format('YYYY-MM-DDTHH:mm:ss.SSS');
+        console.log(timestamp + ' ' + dev.name()
+            + ' len ' + data.length
+            + ' from ' + offset + ' to ' + (offset + data.length - 1));
         console.log(dump(data));
-        var logLine = moment(now).format('YYYY-MM-DDTHH:mm:ss.SSSZZ');
+        var logLine = timestamp;
         logLine += ' ' + dev.name();
         logLine += ' ' + data.toString('hex') + '\n'; 
         if (log) log(logLine);
