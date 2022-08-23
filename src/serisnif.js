5c5b441dd485b7de299a9c85fa0f6eb2831108f6
Woody Wu <woody.wu2@landisgyr.com>
2022-08-23 11:34:35 +0800

    use winston

--- src/serisnif.js
+++ src/serisnif.js
@@ -7,6 +7,102 @@ const EventEmitter = require('events').EventEmitter;
 const { SerialPort } = require('serialport');
 const dump = require('buffer-hexdump');
 const moment = require('moment');
+const { MESSAGE } = require('triple-beam');
+const jsonStringify = require('safe-stable-stringify');
+const logform = require('logform');
+const winston = require('winston');
+
+const DEFAULT_LOG_FILE_MAX_SIZE = 10 * 1024;
+
+const argv = require('yargs/yargs')(process.argv.slice(2))
+    .version('0.0.1')
+    .alias('version', 'v')
+    .help()
+    .alias('help', 'h')
+    .option('left', {
+        alias: 'l',
+        describe: 'left device',
+        type: 'string',
+    })
+    .option('right', {
+        alias: 'r',
+        describe: 'right device',
+        type: 'string',
+    })
+    .option('inter-frame-timeout', {
+        alias: 'i',
+        describe: 'inter-frame-timeout',
+        type: 'numeric',
+        default: 200,
+    })
+    .option('baud', {
+        alias: 'b',
+        describe: 'baud rate',
+        type: 'numeric',
+        default: 115200,
+    })
+    .option('file', {
+        alias: 'f',
+        describe: 'save to file',
+        type: 'string',
+    })
+    .option('max-size', {
+        alias: 's',
+        describe: 'maxsize of each saved log file (in KB)',
+        nargs: 1,
+        type: 'number',
+        default: DEFAULT_LOG_FILE_MAX_SIZE,
+    })
+    .option('left-name', {
+        alias: 'L',
+        describe: 'left device name',
+        type: 'string',
+        default: 'left',
+    })
+    .option('right-name', {
+        alias: 'R',
+        describe: 'right device name',
+        type: 'string',
+        default: 'right',
+    })
+    .argv;
+
+const logFormat = logform.format((info, opts) => {
+    /*
+     * @param {Object} info
+     * @param {string} info.level
+     * @param {Date} info.timestamp
+     * @param {string} info.name
+     * @param {Buffer} info.data
+     * @param {number} info.offset
+     */
+
+    var m = moment(info.timestamp).format('YYYY-MM-DDTHH:mm:ss.SSS')
+        + ' ' + info.name + ' len ' + info.data.length
+        + ' from ' + info.offset + ' to ' + (info.offset + info.data.length - 1);
+    if (opts.multiLines)
+        m += '\n' + dump(info.data);
+    else
+        m += ': ' + info.data.toString('hex');
+
+    info[MESSAGE] = m;
+    return info;
+});
+
+const logger = winston.createLogger({
+    level: 'debug',
+    transports: [
+        new winston.transports.Console({
+            format: logFormat({ multiLines: true }),
+        }),
+    ],
+});
+if (argv.file)
+    logger.add(new winston.transports.File({
+        filename: argv.file,
+        format: logFormat({ multiLines: false }),
+        maxsize: argv.maxSize * 1024,
+    }));
 
 class SerialListener extends EventEmitter {
     constructor(device, baud, interframeTimeout, name) {
@@ -17,7 +113,7 @@ class SerialListener extends EventEmitter {
             baudRate: baud,
             autoOpen: false,
         });
-        this._name = device + (name ? ' ' + name : '');
+        this._name = device + (name ? ' (' + name + ')' : '');
         this.timer = null;
         this._chunk = null;
         this._offset = 0;
@@ -52,99 +148,23 @@ class SerialListener extends EventEmitter {
     }
 }
 
-const makeLogger = file => {
-    const logStream = fs.createWriteStream(file);
-    const queue = [];
-    var buffWaiting = false;
-
-    const write = () => {
-        while (queue.length) {
-            const line = queue.shift();
-            if (! logStream.write(line)) {
-                logStream.once('drain', write);
-                buffWaiting = true;
-                break;
-            }
-            buffWaiting = false;
-        }
-    };
-
-    logStream.on('error', err => {
-        console.error(err.message);
-        process.exit(1);
-    });
-
-    return line => {
-        queue.push(line);
-        if (queue.length > 1 || buffWaiting) return;
-        process.nextTick(write);
-    };
-};
-
-const argv = require('yargs/yargs')(process.argv.slice(2))
-    .version('0.0.1')
-    .alias('version', 'v')
-    .help()
-    .alias('help', 'h')
-    .option('left', {
-        alias: 'l',
-        describe: 'left device',
-        type: 'string',
-    })
-    .option('right', {
-        alias: 'r',
-        describe: 'right device',
-        type: 'string',
-    })
-    .option('inter-frame-timeout', {
-        alias: 'i',
-        describe: 'inter-frame-timeout',
-        type: 'numeric',
-        default: 200,
-    })
-    .option('baud', {
-        alias: 'b',
-        describe: 'baud rate',
-        type: 'numeric',
-        default: 115200,
-    })
-    .option('log', {
-        alias: 'f',
-        describe: 'log file',
-        type: 'string',
-    })
-    .option('left-name', {
-        alias: 'L',
-        describe: 'left device name',
-        type: 'string',
-        default: 'left',
-    })
-    .option('right-name', {
-        alias: 'L',
-        describe: 'right device name',
-        type: 'string',
-        default: 'right',
-    })
-    .argv;
+/*---*/
 
 const left = argv.left
     ? new SerialListener(argv.left, argv.baud, argv.interFrameTimeout, argv.leftName) : null;
 const right = argv.right
     ? new SerialListener(argv.right, argv.baud, argv.interFrameTimeout, argv.rightName) : null;
-const log = argv.log
-    ? makeLogger(argv.log) : null;
 
 [left, right].forEach(dev => {
     if (! dev) return;
     dev.on('data', ({ time, offset, data }) => {
-        const timestamp = moment(time).format('YYYY-MM-DDTHH:mm:ss.SSS');
-        console.log(timestamp + ' ' + dev.name()
-            + ' len ' + data.length
-            + ' from ' + offset + ' to ' + (offset + data.length - 1));
-        console.log(dump(data));
-        var logLine = timestamp;
-        logLine += ' ' + dev.name();
-        logLine += ' ' + data.toString('hex') + '\n'; 
-        if (log) log(logLine);
+        logger.log({
+            level: 'info',
+            timestamp: new Date(),
+            name: dev.name(),
+            data: data,
+            offset: offset,
+            message: '',
+        });
     });
 });
